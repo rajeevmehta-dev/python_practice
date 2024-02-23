e7c582dcdb26a89cec07fefb502d2357ed205181
VeeJar <65169394+rajeevmehta-dev@users.noreply.github.com>
2024-02-23 18:13:01 +0530

    turtle and colorgram module

---
+++ basic_practice.py
@@ -0,0 +1,12 @@
+switch_data = 0
+
+match switch_data:
+    case 0:
+        print("What is your name")
+    case 1:
+        print("I don't know what to do")
+    case 2:
+        print('okay')
+    case default:
+        print("it is not good idea")
+  
